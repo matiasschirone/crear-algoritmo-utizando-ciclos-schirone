292b9311b9551491cd5b754307d5ce65ea8dbc1c
matiasschirone <matiasschirone@gmail.com>
2022-02-17 23:32:31 -0300

    modificar js

--- miarchivo.js
+++ miarchivo.js
@@ -1,60 +1,81 @@
-let nombreProductoA = 'Campo';
-let stockProductoA = 100;
-let precioProductoA = 1000;
-
-let nombreProductoB = 'Popular';
-let stockProductoB = 100;
-let precioProductoB = 1500;
-
-let nombreProductoC = 'Platea';
-let stockProductoC = 100;
-let precioProductoC = 2000;
-alert("Hola estos son los sectores que te podemos ofrecer: \n" + nombreProductoA + "\n" + nombreProductoB + "\n" + nombreProductoC + "\n" + "(Maximo 6 tickets por persona)")
-
+const nombreProductoA = 'Campo';
+const stockProductoA = 100;
+const precioProductoA = 1000;
+ 
+const nombreProductoB = 'Popular';
+const stockProductoB = 80;
+const precioProductoB = 1500;
+ 
+const nombreProductoC = 'Platea';
+const stockProductoC = 60;
+const precioProductoC = 2000;
+ 
 let cantidadComprada
 let precioTotalVenta = 0;
-
-function stockInsuficiente(stock){
-    alert("No tenemos suficiente stock del producto, puede comprar hasta " + stock + ' unidades')
+ 
+ 
+alert(`Hola estos son los sectores que te podemos ofrecer:
+           ${nombreProductoA}
+           ${nombreProductoB}
+           ${nombreProductoC}
+(Maximo 6 tickets por persona)
+`)
+ 
+function stockInsuficiente(stock) {
+   alert("No tenemos suficiente stock del producto, puede comprar hasta " + stock + ' unidades')
 }
-function stocksuficiente(stock, precio, nombre){
-    stock -= cantidadComprada;
-    precioTotalVenta += cantidadComprada * precio;
-    console.log("quedan " + stock + " nombre");
+function stockSuficiente(stock, precio, nombre) {
+   stock -= cantidadComprada;
+   precioTotalVenta += cantidadComprada * precio;
+   console.log("quedan " + stock + " " + nombre + " en stock");
 }
-function compra(stock, precio, nombre){
-    cantidadComprada = parseInt(prompt("ingrese cantidad que quiere comprar"));
-    if (cantidadComprada <= stock, precio, nombre){
-        stocksuficiente(stock, precio, nombre)  
-    }
-    else{
-        stockInsuficiente(stock)
-    }
+function compra(stock, precio, nombre) {
+   cantidadComprada = parseInt(prompt(`Ingrese cantidad que quiere comprar del sector ${nombre}`));
+   if (cantidadComprada <= stock) {
+       stockSuficiente(stock, precio, nombre)
+   }
+   else {
+       stockInsuficiente(stock)
+   }
 }
-let cantidadProductosComprados = parseInt(prompt('ingrese la cantidad de productos distintos que desea comprar'))
-
-for(let i = 0; i < cantidadProductosComprados; i++){
-   
-    let nombreCompra = prompt("ingrese el nombre del producto a comprar:");
-
-    if (nombreCompra == nombreProductoA){
-        compra(stockProductoA, precioProductoA, nombreProductoA)
-    }
-    else if (nombreCompra == nombreProductoB){
-        compra(stockProductoB, precioProductoB, nombreProductoB)
-    }
-    else if (nombreCompra == nombreProductoC){
-        compra(stockProductoC, precioProductoC, nombreProductoC)
-        
-    }
-    else{
-        alert('No tenemos ese producto')
-    }
+ 
+function cuotas() {
+   let cuotas = parseInt(prompt("Ingrese cantidad de cuotas"));
+   let precioCuota = precioTotalVenta / cuotas;
+   alert("El precio de la cuota es de $" + precioCuota.toFixed(2));
 }
-alert("El precio total de su compra es de: $" + precioTotalVenta);
-
-
-
+ 
+let cantidadProductosComprados = parseInt(prompt('Ingrese la cantidad de productos distintos que desea comprar'))
+ 
+for (let i = 0; i < cantidadProductosComprados; i++) {
+ 
+   let nombreCompra = prompt("Ingrese el nombre del producto a comprar:");
+ 
+   if (nombreCompra.toLowerCase() === nombreProductoA.toLowerCase()) {
+       compra(stockProductoA, precioProductoA, nombreProductoA)
+   }
+   else if (nombreCompra.toLowerCase() === nombreProductoB.toLowerCase()) {
+       compra(stockProductoB, precioProductoB, nombreProductoB)
+   }
+   else if (nombreCompra.toLowerCase() === nombreProductoC.toLowerCase()) {
+       compra(stockProductoC, precioProductoC, nombreProductoC)
+ 
+   }
+   else {
+       alert('No tenemos ese producto')
+   }
+}
+ 
+let respuestaCuotas = prompt("¿Desea pagar en cuotas? Si/No");
+ 
+if (respuestaCuotas.toLowerCase() === "si") {
+   cuotas()
+}
+else if (respuestaCuotas.toLowerCase() === "no") {
+   alert("El precio total de la compra es de $" + precioTotalVenta)
+} else {
+   alert("Ingrese una respuesta valida")
+}
 
 
 
